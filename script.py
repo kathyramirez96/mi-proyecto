001b996b8ce75d4f99b1d0f890a325e886302729
kathy Ramirez <kathyramirez96@hotmail.com>
2020-07-02 11:44:40 -0500

    Commit inicial

---
+++ script.py
@@ -0,0 +1,8 @@
+# Script de prueba para Git
+# Autor: Katherine Ramírez <kramirezv@est.ups.edu.ec>
+# Fecha: 02-07-2020
+# Versión: 1.0.0
+def main ():
+	print ( "Hola Mundo!" )	
+if __name__ == "__main__" :
+    main()
