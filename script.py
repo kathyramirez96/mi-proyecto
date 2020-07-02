90e35cc33ab377a1470261f782688effd9048a68
kathy Ramirez <kathyramirez96@hotmail.com>
2020-07-02 11:48:29 -0500

    Agregando datos al proyecto

--- script.py
+++ script.py
@@ -3,6 +3,7 @@
 # Fecha: 02-07-2020
 # Versión: 1.0.0
 def main ():
-	print ( "Hola Mundo!" )	
+	nombre = input( "Cuál es tu nombre? " )
+        print("Hola ",nombre, "Bienvenido a Git!")
 if __name__ == "__main__" :
     main()
